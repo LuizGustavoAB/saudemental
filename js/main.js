6237b74da0c29f12b524b5e5a18c5ff41ec011db
JoaoOVER <99569161+JoaoOVER@users.noreply.github.com>
2022-11-28 10:41:08 -0300

    AP3

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true, /*Os icones de menu do slide*/
+    infinite: true, /*ser ele e infinito ou não*/
+    speed: 300, /*Velosidade que ele passa*/
+    slidesToShow: 1, /*Para mostra quantos slides por vez*/
+    adaptiveHeight: true, /*Ela se adapta ao tamanho da imagem*/
+    autoplay: true,
+    autoplaySpeed: 2000.
+  });
